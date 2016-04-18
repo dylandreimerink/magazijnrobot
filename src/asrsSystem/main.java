7581b3255288d29b210300e7a5a2ba4a6b8f801a
Stevenvdv <vdvsteven3@gmail.com>
2016-04-18 15:21:03 +0200

    as/rs systeem menubar toegevoegd
    acties voor open order en exit zijn toegevoegd.
    open order moet nog een path handler krijgen

--- src/asrsSystem/main.java
+++ src/asrsSystem/main.java
@@ -1,5 +1,8 @@
 package asrsSystem;
 
 public class main {
-
+	public static void main(String args[]){
+	gui scherm = new gui();
+		
+	}
 }
--- src/asrsSystem/product.java
+++ src/asrsSystem/product.java
@@ -1,15 +1,25 @@
 package asrsSystem;
 import java.util.ArrayList;
 
-import Functions.Database;
+import Functions.*;
 
 public class product {
-
-	private ArrayList<product> product;
+	private String naam;
+	private int plaats;
+	private double hoogte;
+	private double breedte;
+	private double lengte;
 	
-	public product(){
+	
+	public product(String naam, int plaats, double hoogte, double breedte, double lengte){
 		Database connection = new Database();
 		connection.connect();
+		this.naam = naam;
+		this.plaats = plaats;
+		this.hoogte = hoogte;
+		this.breedte = breedte;
+		this.lengte = lengte;
 	}
 	
 }
+
