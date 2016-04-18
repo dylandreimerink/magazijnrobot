0128d950a7f5474f06f68cc0a7f377b520a3376a
Stevenvdv <vdvsteven3@gmail.com>
2016-04-18 13:32:02 +0200

    as/rs bestelling integratie

---
+++ src/asrsSystem/order.java
@@ -0,0 +1,21 @@
+package asrsSystem;
+import java.util.ArrayList;
+
+import Functions.*;
+
+public class order {
+
+	private String path="";
+	private ArrayList<Product> product;
+	
+	
+	public order(String path) {
+		this.path = path;
+		Bestelling order = new Bestelling(this.path);
+		product = new ArrayList();
+		product = order.getProductList();
+	}
+	public ArrayList<Product> getProductList() {
+		return product;
+	}
+}
