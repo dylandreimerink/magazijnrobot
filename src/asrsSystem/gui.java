14d370ddd01134da7d3d7e5c92df7f58106e3713
Stevenvdv <vdvsteven3@gmail.com>
2016-04-18 15:21:58 +0200

    as/rs menubar update

--- src/asrsSystem/gui.java
+++ src/asrsSystem/gui.java
@@ -1,5 +1,128 @@
 package asrsSystem;
 
-public class gui {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+import com.sun.glass.events.KeyEvent;
+import com.sun.xml.internal.ws.api.Component;
+
+import javax.swing.KeyStroke;
+import java.awt.event.InputEvent;
+
+public class gui extends JFrame implements ActionListener {
+
+	
+
+	   
+	public gui() {
+		
+		// initialiseer en maak menuopties.
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnBestand = new JMenu("Bestand");
+		menuBar.add(mnBestand);
+		
+		
+		// open order - path handler toevoegen
+		JMenuItem mntmOpenOrder = new JMenuItem("Open order");
+		mntmOpenOrder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ChooseFile();
+			}
+		});
+		mntmOpenOrder.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		mnBestand.add(mntmOpenOrder);
+		
+		
+		//open recent NOT DONE
+		JMenu mnOpenRecent = new JMenu("Open recent");
+		mnOpenRecent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnBestand.add(mnOpenRecent);
+		
+		
+		//exit DONE
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		mntmExit.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, InputEvent.CTRL_MASK));
+		mnBestand.add(mntmExit);
+		
+		// voeg artikel toe NOT DONE
+		JMenuItem mntmVoegArtikelToe = new JMenuItem("Voeg artikel toe");
+		mntmVoegArtikelToe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnBestand.add(mntmVoegArtikelToe);
+		
+		// verwijder artikel NOT DONE
+		JMenuItem mntmVerwijderArtikel = new JMenuItem("Verwijder artikel");
+		mntmVerwijderArtikel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnBestand.add(mntmVerwijderArtikel);
+		
+		// bewerk artikel NOT DONE
+		JMenuItem mntmBewerkArtikel = new JMenuItem("Bewerk artikel");
+		mntmBewerkArtikel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnBestand.add(mntmBewerkArtikel);
+		
+		//scherm opbouwen
+		JFrame frame = new JFrame();
+        this.setSize(1200,900);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("AS/RS Systeem");
+        getContentPane().setLayout(new FlowLayout());
+        
+        this.setVisible(true);
+	}
+        
+
+	//filechooser functie, geeft path van het gekozen bestand terug, path moet nog op een andere manier verwerkt worden.
+	public void ChooseFile() {
+	    JFileChooser chooser = new JFileChooser();
+	    chooser.setCurrentDirectory(new java.io.File("."));
+	    chooser.setDialogTitle("kies uw XML file");
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    chooser.setAcceptAllFileFilterUsed(false);
+
+	    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+	      System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
+	      System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+	    } else {
+	      System.out.println("No Selection ");
+	    }
+	  }
+	
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
 
 }
