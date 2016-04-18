5aed71f66dbbca8403e67364d9463d0d306c3031
mikeveltman <mikeveltman@live.nl>
2016-04-18 11:48:17 +0200

    Scherm gemaakt
    GUI

--- src/bppSimulator/Main.java
+++ src/bppSimulator/Main.java
@@ -3,8 +3,7 @@ package bppSimulator;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainGUI mainWindow = new MainGUI();
 	}
 
 }
---
+++ src/bppSimulator/SimulatiePanel.java
@@ -0,0 +1,19 @@
+package bppSimulator;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class SimulatiePanel extends JPanel {
+
+	public SimulatiePanel(MainGUI parant) {
+		parant.lbltijd_2.setText("55 sec");
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+	}
+	
+}
