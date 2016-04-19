fe422abfe4bbc71db2fc5c393e72ced248d455c8
rmastenbroek18 <rmastenbroek18@gmail.com>
2016-04-19 13:26:08 +0200

    JDialoog voor artikel

---
+++ src/asrsSystem/Artikel.java
@@ -0,0 +1,43 @@
+package asrsSystem;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class Artikel extends JDialog implements ActionListener {
+	
+	private JButton create;
+	private JButton edit;
+	private JButton delete;
+	
+	Artikel(JFrame frame){
+		super(frame, true);
+		setLayout(new FlowLayout());
+		create = new JButton("Create Artikel");
+		edit = new JButton("Edit Artikel");
+		delete = new JButton ("Delete Artikel");
+		add(create);
+		add(edit);
+		add(delete);
+		create.addActionListener(this);
+		edit.addActionListener(this);
+		delete.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == create){
+			//Vervolg
+		}
+		else if (e.getSource() == edit){
+			//Vervolg
+		}
+		else if (e.getSource() == delete){
+			//Vervolg
+		}
+	}
+
+}
